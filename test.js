9b627f8cc9b9efc013389976416317f28b84f0e8
Leon Si <leon@leondreamed.com>
2023-09-30 17:15:14 -0400

    fix: tests

--- test.js
+++ test.js
@@ -1,48 +1,53 @@
-import fs from 'fs';
-import path from 'path';
-import isJpg from 'is-jpg';
-import pify from 'pify';
-import test from 'ava';
-import m from './';
+import fs from "fs";
+import isJpg from "is-jpg";
+import test from "ava";
+import m from "./index.js";
+import { join, filename } from "desm";
 
-const fsP = pify(fs);
+test("extract file", async (t) => {
+  const buf = await fs.promises.readFile(
+    join(import.meta.url, "fixtures", "file.zip")
+  );
+  const files = await m()(buf);
 
-test('extract file', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures', 'file.zip'));
-	const files = await m()(buf);
-
-	t.is(files[0].path, 'test.jpg');
-	t.true(isJpg(files[0].data));
+  t.is(files[0].path, "test.jpg");
+  t.true(isJpg(files[0].data));
 });
 
-test('extract multiple files', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures', 'multiple.zip'));
-	const files = await m()(buf);
+test("extract multiple files", async (t) => {
+  const buf = await fs.promises.readFile(
+    join(import.meta.url, "fixtures", "multiple.zip")
+  );
+  const files = await m()(buf);
 
-	t.is(files.length, 4);
-	t.is(files[0].path, '0.txt');
-	t.is(files[0].type, 'file');
-	t.is(files[0].data.toString(), '1');
-	t.is(files[3].path, '3/4/');
-	t.is(files[3].type, 'directory');
+  t.is(files.length, 4);
+  t.is(files[0].path, "0.txt");
+  t.is(files[0].type, "file");
+  t.is(files[0].data.toString(), "1");
+  t.is(files[3].path, "3/4/");
+  t.is(files[3].type, "directory");
 });
 
-test('extract symlinks', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures', 'symlink.zip'));
-	const files = await m()(buf);
+test("extract symlinks", async (t) => {
+  const buf = await fs.promises.readFile(
+    join(import.meta.url, "fixtures", "symlink.zip")
+  );
+  const files = await m()(buf);
 
-	t.is(files[0].path, 'ReactiveCocoa');
-	t.is(files[0].type, 'symlink');
-	t.is(files[0].linkname, 'Versions/Current/ReactiveCocoa');
+  t.is(files[0].path, "ReactiveCocoa");
+  t.is(files[0].type, "symlink");
+  t.is(files[0].linkname, "Versions/Current/ReactiveCocoa");
 });
 
-test('return empty array if non-valid file is supplied', async t => {
-	const buf = await fsP.readFile(__filename);
-	const files = await m()(buf);
+test("return empty array if non-valid file is supplied", async (t) => {
+  const buf = await fs.promises.readFile(filename(import.meta.url));
+  const files = await m()(buf);
 
-	t.is(files.length, 0);
+  t.is(files.length, 0);
 });
 
-test('throw on wrong input', async t => {
-	await t.throws(m()('foo'), 'Expected a Buffer, got string');
+test("throw on wrong input", async (t) => {
+  await t.throwsAsync(() => m()("foo"), {
+    message: "Expected a Buffer, got string",
+  });
 });
